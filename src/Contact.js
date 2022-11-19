a4b85cfd9937989067ee87c52236f302f6983e39
Tturna <eemelijuponaho@gmail.com>
2022-11-19 21:18:43 +0200

    init

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact us</h2>
+                <p>If you're interested in getting yourself a shiny new tractor, don't hesitate to contact us at:</p>
+                <p>email: support(at)superco.org</p>
+                <p>tel. 1-205-133-0514</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
